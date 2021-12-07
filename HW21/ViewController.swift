c87d511f1150f0506d51c3caaa9bae6330abd846
bsergiov <81264661+bsergiov@users.noreply.github.com>
2021-12-07 09:37:41 +0200

    Initial Commit

---
+++ HW21/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW21
+//
+//  Created by Сергей Бровко on 07.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
